5338f7abea8eb58131838bf559b0436787585c50
sreedhar742 <psridhar742@gmail.com>
2024-05-26 18:22:47 +0530

    mail change

--- rentify/settings.py
+++ rentify/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "psridhar742@gmail.com"
-EMAIL_HOST_PASSWORD = "7095SREE@psr"
+EMAIL_HOST_PASSWORD = "password"
 
 # Application definition
 
